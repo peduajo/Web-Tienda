8a8b2b0dd079bebbd3e3af0602e672b4509581a8
Eduardo <peduajo@gmail.com>
2018-05-07 00:29:52 +0200

    anuncios añadidos

--- SOURCE/app.py
+++ SOURCE/app.py
@@ -1,19 +1,12 @@
 # encoding: utf-8
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template
 import os
-import io
-
 
 app = Flask(__name__)
-
 
 @app.route('/tienda')
 def tienda():
 	return render_template('tienda.html')
-
-@app.route('/noticias')
-def noticias():
-	return render_template('noticias.html')
 
 @app.route('/sobre_nosotros')
 def sobre_nosotros():
@@ -22,7 +15,6 @@ def sobre_nosotros():
 @app.route('/contacto')
 def contacto():
     return render_template('ubicacion.html')
-
 
 if __name__=="__main__":
     app.secret_key = os.urandom(12)
