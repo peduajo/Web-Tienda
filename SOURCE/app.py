3665a019a194863115aa2155c46a729dcca459d7
Eduardo <peduajo@gmail.com>
2018-05-12 15:43:26 +0200

    no resize

--- SOURCE/app.py
+++ SOURCE/app.py
@@ -1,4 +1,3 @@
-# encoding: utf-8
 from flask import Flask,render_template
 import os
 
@@ -17,5 +16,4 @@ def contacto():
     return render_template('ubicacion.html')
 
 if __name__=="__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True)
+    app.run()
